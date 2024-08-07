bf89ea1713db7995376bfb2475ea74f70b5773a9
Gurukiran Patrimath <gurukiranp@infinite.com>
2024-08-07 11:19:55 +0530

    CCA8

---
+++ Web Technolgies/Assessement/CCA8/CCA8/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CCA8
+{
+     class Program
+    {
+        static void Main()
+        {
+        }
+    }
+}
