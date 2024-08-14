25e709883af9e3062fb0081763eae0d775a273ec
Gurukiran Patrimath <gurukiranp@infinite.com>
2024-08-14 11:49:55 +0530

    MVC

---
+++ MVC/Assessement/MVC/MVC/Controllers/CodeController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI.WebControls;
+using System.Web.Mvc;
+
+namespace MVC.Controllers
+{
+    public class CodeController : Controller
+    {
+        private readonly NorthwindDBEntities _context;
+        public CodeController()
+        {
+            _context = new NorthwindDBEntities();
+        }
+        public ActionResult CustomersInGermany()
+        {
+            var customers = _context.Customers
+                                    .Where(c => c.Country == "Germany")
+                                    .Select(c => new Customers
+                                    {
+                                        CustomerID = c.CustomerID,
+                                        CompanyName = c.CompanyName,
+                                        ContactName = c.ContactName,
+                                        City = c.City,
+                                        Country = c.Country
+                                    }).ToList();
+            return View(customers);
+        }
+
+        public ActionResult CustomerDetailsForOrder()
+        {
+            var customer = (from o in _context.Orders
+                            join c in _context.Customers on o.CustomerID equals c.CustomerID
+                            where o.OrderID == 6805
+                            select new Customers
+                            {
+                                CustomerID = c.CustomerID,
+                                CompanyName = c.CompanyName,
+                                ContactName = c.ContactName,
+                                City = c.City,
+                                Country = c.Country
+                            }).FirstOrDefault();
+            return View(customer);
+        }
+    }
+}
---
+++ MVC/Assessement/MVC/MVC/Controllers/Customers.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVC.Controllers
+{
+    public class Customers
+    {
+        public string CustomerID { get; set; }
+        public string CompanyName { get; set; }
+        public string ContactName { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+    }
+}
